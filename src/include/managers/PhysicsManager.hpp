b9b428eebe538020f462a5b7176479a89f4f9390
Unknown <gached@gmail.com>
2018-04-19 20:13:16 +0200

    merge

--- src/include/managers/PhysicsManager.hpp
+++ src/include/managers/PhysicsManager.hpp
@@ -108,13 +108,10 @@ public:
     void                    fastGravity(int p_idBody);
     void                    machineGun(int p_idBody, int p_orientation);
 
-<<<<<<< HEAD
     void                    getPosition(int p_idBody);
     void                    move(int p_idBody, float p_moveX, float p_moveY);
     void                    jump(int p_idBody, float p_force);
-=======
     int                     getTotalFixtures(int p_idBody);
->>>>>>> debuug
 
 };
 
